5032d33f19358e4b27d4ecadbe3745b96cd6c6a9
duc Tri <ductri02lt2@gmail.com>
2026-01-07 09:22:23 +0700

    first commit

---
+++ assignment 1/assignment-1/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>This is app</h1>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ assignment 1/assignment-1/src/view/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home container py-5">
+    <h1>Welcome to My Vue App!</h1>
+    <p>This is the home page for your production-ready Vue project.</p>
+    <button @click="clickMe" class="btn btn-primary">Click Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    clickMe() {
+      alert('Hello from Home.vue!')
+    },
+  },
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
