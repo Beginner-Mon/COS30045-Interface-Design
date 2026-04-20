9cf05f18c20e922b133e9baea63f80dc5a90bb21
Tri Tran <142661626+Beginner-Mon@users.noreply.github.com>
2026-04-20 23:51:06 +0700

    Update base path in vite.config.js
    Changed the base path in vite.config.js from a hardcoded value to a relative path.

--- assignment-1/vite.config.js
+++ assignment-1/vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/COS30045-Interface-Design/'   // ← Hardcoded for your repo
+  base: './'   
 })
